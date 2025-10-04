7fa960d065f0e834b001a708c4620fe0c719f86a
Weixiang Gao <gaowx2003@gmail.com>
2025-10-04 23:55:04 +0800

    Update humanoid_amp.py

--- phc/env/tasks/humanoid_amp.py
+++ phc/env/tasks/humanoid_amp.py
@@ -532,6 +532,33 @@ class HumanoidAMP(Humanoid):
         # if flags.debug:
         # print('raising for debug')
         # root_pos[..., 2] += 0.5
+        if MODIFY:
+            while True:
+                env_ids = torch.from_numpy(np.arange(self.num_envs)).to(self.device)
+                dof_pos = self.data_coll["dof_pos"][self.play_index]
+                rb_pos = self.data_coll["ref_rb_pos"][self.play_index]
+                rb_rot = self.data_coll["ref_rb_rot"][self.play_index]
+                body_vel = self.data_coll["ref_body_vel"][self.play_index]
+                body_ang_vel = self.data_coll["ref_body_ang_vel"][self.play_index]
+                root_pos = rb_pos[0]
+                root_rot = rb_rot[0]
+                root_vel = body_vel[0]
+                root_ang_vel = body_ang_vel[0]
+                self.play_index += 1
+                if self.play_index == len(self.data_coll["dof_pos"]):
+                    self.play_index = 0
+                self._set_env_state(env_ids=env_ids, root_pos=root_pos, root_rot=root_rot, 
+                                    dof_pos=dof_pos, root_vel=root_vel, root_ang_vel=root_ang_vel, 
+                                    dof_vel=dof_vel, rigid_body_pos=rb_pos, rigid_body_rot=rb_rot, 
+                                    rigid_body_vel=body_vel, rigid_body_ang_vel=body_ang_vel)
+                self._reset_env_tensors(env_ids)
+                self.gym.simulate(self.sim)
+                if self.device == 'cpu':
+                    self.gym.fetch_results(self.sim, True)
+                self._refresh_sim_tensors()
+                self.render()
+
+
 
         # if flags.fixed:
         #     x_grid, y_grid = torch.meshgrid(torch.arange(64), torch.arange(64))
