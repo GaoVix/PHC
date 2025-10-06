60a6225f56263e1966625d3962c76e9a6c8f0f8e
Weixiang Gao <gaowx2003@gmail.com>
2025-10-07 00:50:37 +0800

    Update motion_lib.py

--- phc/utils/motion_lib.py
+++ phc/utils/motion_lib.py
@@ -276,8 +276,8 @@ class MotionLib():
         body_ang_vel = torch.cat([torch.as_tensor(m['body_ang_vel']) for m in _motions], dim=0).float().to(self._device)
         body_ang_vel_global = []
         for i in range(self.body_pos.shape[1]):
-            body_ang_vel_global.append(quat_rotate(torch.as_tensor(self.body_quat[:,i,:]), torch.as_tensor(body_ang_vel[:,i,:])))
-        self.body_ang_vel = torch.cat(body_ang_vel_global, dim=0)
+            body_ang_vel_global.append((quat_rotate(torch.as_tensor(self.body_quat[:,i,:]), torch.as_tensor(body_ang_vel[:,i,:]))).unsqueeze(1))
+        self.body_ang_vel = torch.cat(body_ang_vel_global, dim=1)
         print(self.body_ang_vel.shape)
         assert self.body_ang_vel.shape[-2] == 38
         # self.body_ang_vel = torch.cat([quat_rotate(torch.as_tensor(m['body_quat']), torch.as_tensor(m['body_ang_vel'])) for m in _motions], dim=0).float().to(self._device)
