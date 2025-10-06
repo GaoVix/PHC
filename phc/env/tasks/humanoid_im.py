2877663c64c866c0a8bee6c9d10f96904caab11f
Weixiang Gao <gaowx2003@gmail.com>
2025-10-07 00:39:08 +0800

    Update humanoid_im.py

--- phc/env/tasks/humanoid_im.py
+++ phc/env/tasks/humanoid_im.py
@@ -123,7 +123,7 @@ class HumanoidIm(humanoid_amp_task.HumanoidAMPTask):
         self.vis_ref = True
         self.vis_contact = False
         # self._sampled_motion_ids = torch.arange(self.num_envs).to(self.device)
-        self._sampled_motion_ids = torch.zeros(self.num_envs).to(self.device)
+        self._sampled_motion_ids = torch.zeros(self.num_envs, dtype=torch.long).to(self.device)
         self.create_o3d_viewer()
         self.save_flag=True
         self.motion_res_history = []
