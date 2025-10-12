45b1bebecf68c5b206f50bff3e34949e3d15d314
Weixiang Gao <gaowx2003@gmail.com>
2025-10-12 22:59:00 +0800

    Update humanoid_im_distill.py

--- phc/env/tasks/humanoid_im_distill.py
+++ phc/env/tasks/humanoid_im_distill.py
@@ -204,13 +204,7 @@ class HumanoidImDistill(humanoid_im.HumanoidIm):
                             weights = self.composer(full_obs)
                             gt_action = torch.sum(weights[:, :, None] * x_all, dim=1)
                         else:
-                            gt_action, _ = self.pnn(full_obs)
-                            for i in gt_action:
-                                print(i.shape)
-                                print(777)
-                            print('----------------------')
-                            print(_.shape)
-                            raise RuntimeError
+                            gt_action, _ = self.pnn(full_obs, idx=0)
                     else:
                         gt_action = self.encoder(full_obs)
                         # x_all = torch.stack([net(full_obs) for net in self.actors], dim=1)
